554c63ae0eb95449f9d2e7147b978f7f5407a26f
billyprice98 <billyprice98is@gmail.com>
2019-03-13 23:21:59 +1100

    better initial commit lol

--- hexboard.py
+++ hexboard.py
@@ -1,23 +1,48 @@
 from enum import Enum
+import itertools
 
-SIZE = 4
+PRINT_COORDS = True
+
+SIZE = 3
 
 class Tile(Enum):
-    BLANK = 0
-    RED   = 1
-    GREEN = 2
-    BLUE  = 3
+    BLANK, RED, GREEN, BLUE = range(4)
 
 
 class HexBoard():
     def __init__(self, size=SIZE):
         self.size = size
-        self.grid = {(x,y,z) : Tile.BLANK   for x in range(-self.size+1, self.size)
-                                            for y in range(-self.size+1, self.size)
-                                            for z in range(-self.size+1, self.size) if sum((x,y,z))==0}
+        self.grid = {coord : Tile.BLANK for coord in self.iter_coords()}
+
+    def iter_coords(self):
+        for x in range(-self.size, self.size+1):
+            low = max(- x - self.size, -self.size)
+            hih = min(- x + self.size,  self.size)
+            for y in range(low, hih+1):
+                yield (x,y,-x-y)
+
+    def __iter__(self):
+        for coord in self.iter_coords():
+            yield self.grid[coord]
+
+    def iter_rows(self):
+        for x in range(-self.size, self.size+1):
+            low = max(- x - self.size, -self.size)
+            hih = min(- x + self.size,  self.size)
+            yield (self.grid[x,y,-x-y] for y in range(low, hih+1))
+
+    def __repr__(self):
+        result = ''
+        for i, row in enumerate(self.iter_rows()):
+            result += '\n' + ' '*abs(i-board.size) + ' '.join(str(tile) for tile in row)
+        return result
 
 if (__name__ == "__main__"):
     board = HexBoard()
-    for coord in sorted(board.grid.keys()):
-        print(coord)
+    print(board)
 
+# -3 ->  0, 3
+# -2 -> -1, 3
+# -1 -> -2, 3
+#  0 -> -3, 3
+#  1 -> -3, 
